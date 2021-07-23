9d5d92de723b9a8fa207bb94f15ee4d68eb87101
Dakota Walsh <kota@nilsu.org>
2021-07-23 23:15:35 +1200

    initial commit

---
+++ cmd/forecast.go
@@ -0,0 +1,61 @@
+/*
+Copyright © 2021 Dakota Walsh <kota@nilsu.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"git.sr.ht/~kota/metservice-go"
+	"github.com/spf13/cobra"
+)
+
+// forecastCmd represents the forecast command
+var forecastCmd = &cobra.Command{
+	Use:   "forecast",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: Forecast,
+}
+
+func init() {
+	rootCmd.AddCommand(forecastCmd)
+}
+
+func Forecast(cmd *cobra.Command, args []string) {
+	client := metservice.NewClient()
+	ctx := context.Background()
+
+	forecast, _, err := client.GetForecast(ctx, "Dunedin")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(*forecast.LocationIPS)
+	for _, day := range forecast.Days {
+		fmt.Printf("%v\nforecast: %v\nmax: %vC\nmin: %vC\n\n",
+			*day.Date,
+			*day.ForecastWord,
+			*day.Max,
+			*day.Min)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module git.sr.ht/~kota/metweather
+
+go 1.16
+
+require (
+	git.sr.ht/~kota/metservice-go v1.0.0
+	github.com/spf13/cobra v1.2.1
+)
