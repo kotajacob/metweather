3597e47134b81a959465d7231b7990d0fbbb978d
Dakota Walsh <kota@nilsu.org>
2021-08-11 14:11:47 +1200

    update forecast_test with new output

--- cmd/forecast_test.go
+++ cmd/forecast_test.go
@@ -28,16 +28,16 @@ func TestForecastWeekly(t *testing.T) {
 		t.Errorf("metweather.forecastWeekly returned error: %v", err)
 	}
 	got := out.(*bytes.Buffer).String()
-	want := `2021-7-16 Partly cloudy 7-13°C
-2021-7-17 Showers 8-11°C
-2021-7-18 Few showers 7-10°C
-2021-7-19 Showers 4-11°C
-2021-7-20 Partly cloudy 6-11°C
-2021-7-21 Partly cloudy 6-12°C
-2021-7-22 Few showers 6-11°C
-2021-7-23 Fine 5-10°C
-2021-7-24 Fine 5-11°C
-2021-7-25 Partly cloudy 6-12°C
+	want := `2021-7-16 7-13°C Partly cloudy
+2021-7-17 8-11°C Showers
+2021-7-18 7-10°C Few showers
+2021-7-19 4-11°C Showers
+2021-7-20 6-11°C Partly cloudy
+2021-7-21 6-12°C Partly cloudy
+2021-7-22 6-11°C Few showers
+2021-7-23 5-10°C Fine
+2021-7-24 5-11°C Fine
+2021-7-25 6-12°C Partly cloudy
 `
 	if got != want {
 		t.Errorf("metweather.forecastWeekly\ngot = %q\nwant = %q", got, want)
